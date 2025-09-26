768bf7bfea693f86a40c1c85902ca8822ed81364
Dan K. Snelson <24623109+dan-snelson@users.noreply.github.com>
2025-09-26 08:32:32 -0400

    Assembled Fri Sep 26 08:32:32 EDT 2025
    Signed-off-by: Dan K. Snelson <24623109+dan-snelson@users.noreply.github.com>

--- Uninstallomator.sh
+++ Uninstallomator.sh
@@ -71,7 +71,7 @@ if [[ $(/usr/bin/arch) == "arm64" ]]; then
         rosetta2=no
     fi
 fi
-VERSION="1.1.3"
+VERSION="1.1.4"
 VERSIONDATE="2025-09-26"
 
 
@@ -207,8 +207,9 @@ list_user_homes(){
   | sort -u | grep -E '^/Users/[^/]+' | grep -vE '^/Users/(Shared|Guest)$'
 }
 expand_user_path(){
-  local tpl="$1" home="$2"
-  printf '%s' "${tpl//%USER_HOME%/$home}"
+  local tpl="$1" home="$2" pattern="%USER_HOME%"
+  setopt localoptions sh_glob
+  printf '%s' "${tpl//${pattern}/$home}"
 }
 
 # --- Uninstall engine (runs after label case sets arrays) ---
@@ -562,7 +563,8 @@ intellijidea)
     agents=()
     daemons=()
     profiles=()
-;;macwhisper)
+;;
+macwhisper)
     app_name="MacWhisper"
     bundle_id="com.goodsnooze.MacWhisper"
     app_paths=(
@@ -771,7 +773,8 @@ whispertranscription)
     agents=()
     daemons=()
     profiles=()
-;;zoomus)
+;;
+zoomus)
     app_name="zoom.us"
     bundle_id="us.zoom.xos"
     app_paths=(
