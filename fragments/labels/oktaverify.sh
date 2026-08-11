3c712bc86535d5120641675d709d581625a1f521
Dan K. Snelson <24623109+dan-snelson@users.noreply.github.com>
2026-08-11 16:37:43 -0400

    New Label: Okta Verify

---
+++ fragments/labels/oktaverify.sh
@@ -0,0 +1,24 @@
+oktaverify)
+    app_name="Okta Verify"
+    bundle_id="com.okta.mobile"
+    app_paths=(
+      "/Applications/Okta Verify.app"
+    )
+    pkgs=(
+      "com.okta.mobile"
+    )
+    files=()
+    user_files=(
+      "%USER_HOME%/Library/Application Support/Okta Verify"
+      "%USER_HOME%/Library/Preferences/com.okta.mobile.plist"
+      "%USER_HOME%/Library/Caches/com.okta.mobile"
+      "%USER_HOME%/Library/Logs/Okta Verify"
+      "%USER_HOME%/Library/Group Containers/B7F62B65B*"
+    )
+    agents=()
+    daemons=(
+      "/Library/LaunchDaemons/com.okta.authentication.service.plist"
+      "/Library/LaunchDaemons/com.okta.deviceaccess.servicedaemon.plist"
+    )
+    profiles=()
+;;
